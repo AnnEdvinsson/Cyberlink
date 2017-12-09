a85c79a5362584baae26bf80de03d53cf5e5f24a
Ann Edvinsson <aslin.ann@gmail.com>
2017-12-09 17:10:13 +0100

    updating login

--- login.php
+++ login.php
@@ -3,21 +3,24 @@
 <article>
     <h1>Login</h1>
 
-    <form action="app/auth/login.php" method="post">
+    <form action="app/auth/alogin.php" method="post">
         <div class="form-group">
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" placeholder="francis@darjeeling.com" required>
-            <small class="form-text text-muted">Please provide the your email address.</small>
+            <input class="form-control" type="email" name="email" placeholder="email" required>
+            <small class="form-text text-muted">Please provide your email address.</small>
         </div><!-- /form-group -->
 
         <div class="form-group">
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" required>
-            <small class="form-text text-muted">Please provide the your password (passphrase).</small>
+            <small class="form-text text-muted">Please provide your password.</small>
         </div><!-- /form-group -->
 
         <button type="submit" class="btn btn-primary">Login</button>
     </form>
+    <small class="form-text text-muted">First time on Cyberlink?</small>
+    <a href="signup.php">Sign up here</a>
+
 </article>
 
 <?php require __DIR__.'/views/footer.php'; ?>
