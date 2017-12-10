4334a0c9379debe26c68c0aaf97df39bd7332bb0
Ann Edvinsson <aslin.ann@gmail.com>
2017-12-10 11:50:58 +0100

    adding error array

--- app/auth/alogin.php
+++ app/auth/alogin.php
@@ -4,36 +4,46 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
+$errors = [];
+
 // Check if both email and password exists in the POST request.
 if (isset($_POST['email'], $_POST['password'])) {
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-    // Prepare, bind email parameter and execute the database query.
-    $statement = $pdo->prepare('SELECT * FROM users WHERE email = :email');
-    $statement->bindParam(':email', $email, PDO::PARAM_STR);
-    $statement->execute();
+    // The trim function removes any unwanted spaces.
+    $email =trim($_POST['email']);
 
-    // Fetch the user as an associative array.
-    $user = $statement->fetch(PDO::FETCH_ASSOC);
-
-    // If we couldn't find the user in the database, redirect back to the login
-    // page with our custom redirect function.
-    if (!$user) {
-        redirect('/signup.php');
+    //Check if the inputed value is empty or not.
+    if(empty($email)) {
+        $errors[] ='The email field is empty';
     }
 
-    // If we found the user in the database, compare the given password from the
-    // request with the one in the database using the password_verify function.
-    if (password_verify($_POST['password'], $user['password'])) {
-        // If the password was valid we know that the user exists and provided
-        // the correct password. We can now save the user in our session.
-        // Remember to not save the password in the session!
-        unset($user['password']);
+    //filter_var function to validate the input data. If the
+    // function returns false the value of $_POST['email'] isn't
+    // a valid email address.
+    if (!filter_var($_POST['email'], FILTER_SANITIZE_EMAIL)) {
+        $errors[] ='The email is not a valid email address.';
+    }
+    $password = filter_var($_POST['password']);
 
+    // Prepare, bind email parameter and execute the database query.
+    $checkUser = $pdo->prepare("SELECT email FROM users WHERE email=:email");
+    $checkUser ->bindParam(':email', $email, PDO::PARAM_STR);
+    $checkUser -> execute();
+    // Fetch the result as an associative array.
+    $user = $checkUser->fetch(PDO::FETCH_ASSOC);
+
+    // If the user is not in the database, the user will be redirected to
+    // the signup page with the redirect function.
+    if(!$user) {
+        redirect("../../signup.php");
+    }
+    //If the user is in the database, the given password from the request
+    // is compared with the one in the database using the password_verify function.
+    if (password_verify($_POST['password'], $user['password'])) {
+        //If password is correct the user but not the password will be saved in the session.
+        unset($user['password']);
         $_SESSION['user'] = $user;
     }
 }
-
-// We should put this redirect in the end of this file since we always want to
-// redirect the user back from this file. We don't know
+// redirect the user
 redirect('/');
