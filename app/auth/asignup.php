bd15547da109828fa28e444f1a80799572b38512
Ann Edvinsson <aslin.ann@gmail.com>
2017-12-10 18:02:59 +0100

    rewriting isset

--- app/auth/asignup.php
+++ app/auth/asignup.php
@@ -4,39 +4,59 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-if (isset($_POST['submit'])) {
+$errors = [];
+
+if (isset($_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['userName'], $_POST['pwd'], $_POST['bio'], $_POST['avatar'])) {
+    //check if input chacters are valid
+    $firstName = trim(filter_var($_POST['firstName'], FILTER_SANITIZE_SPECIAL_CHARS));
+    $lastName = trim(filter_var($_POST['lastName'], FILTER_SANITIZE_SPECIAL_CHARS));
+    $email = trim($_POST['email']);
+    //check if email is invalid
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'The email is not a valid email address.';
+    }
+    $userName = trim(filter_var($_POST['userName'], FILTER_SANITIZE_SPECIAL_CHARS));
+    $pwd = trim(filter_var($_POST['pwd'], FILTER_SANITIZE_SPECIAL_CHARS));//DB vilka krav här?
+    $bio = trim(filter_var($_POST['bio'], FILTER_SANITIZE_SPECIAL_CHARS));
+    $avatar = $_FILES['avatar'];
+    //check if the avatar image is the right file type
+    if (!in_array($avatar['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
+        $errors[] = 'The file type is not allowed';
+    }
+    //check if the avatar image is the right file size, maximum 2MB
+    if($avatar['size'] > 2000000) {
+        $error[] = 'The file is larger than 2 megabyte.';
+    }
+    // If there are any errors in the array, upload the file. DB ska vi uploada el till db?
+    if (count($errors) === 0) {
+        $destination = __DIR__.'/uploads/'.$avatar['name'];//DB vilken är den korrekta sökvägen?
+    //upload file from the temporary path to a new destination.
+          move_uploaded_file($avatar['tmp_name'], $destination);
+    }
+
+
+
+
     //include the database file.
-    //
-    //get the data from the signup form.
-    $first = mysqli_real_escape_string($pdo, $_POST['first']);
-    $last = mysqli_real_escape_string($pdo, $_POST['last']);
+    //DB
+    //get the data from the asignup form.
+    $firstName = mysqli_real_escape_string($pdo, $_POST['firstName']);
+    $lastName = mysqli_real_escape_string($pdo, $_POST['lastName']);
     $email = mysqli_real_escape_string($pdo, $_POST['email']);
-    $uid = mysqli_real_escape_string($pdo, $_POST['uid']);
+    $userName = mysqli_real_escape_string($pdo, $_POST['userName']);
     $pwd = mysqli_real_escape_string($pdo, $_POST['pwd']);
     $bio = mysqli_real_escape_string($pdo, $_POST['bio']);
     $avatar = mysqli_real_escape_string($pdo, $_POST['avatar']);
 
-    //error handlers to check that the user insert data correctly
-    //check for empty fields
-    if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
-        redirect("../../signup.php");
-    } else {
-        //check if input chacters are valid
-        if(!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)){
-            redirect("../../signup.php");
-        } else {
-            //check if email is invalid
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                redirect("../../signup.php");
-            } else {
+
                 //connect to database to check if the username already exist in the database
-                //
+                //DB
                 //return result from database
                 // $result = sqlite ????($pd, $sql);
-                //
+                //DB
                 //check if we have any results
                 // $resultCheck = sqlite????($result);
-                //
+                //DB
                 //check if the user id is taken
                 if ($resultCheck > 0) {
                     redirect("../../signup.php");
@@ -48,14 +68,6 @@ if (isset($_POST['submit'])) {
                     //take the user back to the login page
                     redirect("../../login.php");
                 }
-            }
-        }
-    }
-} else {
+            } else {
     redirect("../../login.php");
 }
-
-
-
-
-// redirect('/');
