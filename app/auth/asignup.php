30af72f3b9dfa64fb9a61ac8df8346f508afb4f9
Ann Edvinsson <aslin.ann@gmail.com>
2017-12-12 16:35:40 +0100

    updating asignup.php

--- app/auth/asignup.php
+++ app/auth/asignup.php
@@ -4,70 +4,56 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
+// $statement = $pdo->query('SELECT * FROM users'); //ska jag välja hela users?
+// $?? = $statement->fetchAll(PDO::FETCH_ASSOC);  FF vad ska jag sätta in här
+
 $errors = [];
 
-if (isset($_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['userName'], $_POST['pwd'], $_POST['bio'], $_POST['avatar'])) {
+if (isset($_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['userName'], $_POST['password'])) {
     //check if input chacters are valid
     $firstName = trim(filter_var($_POST['firstName'], FILTER_SANITIZE_SPECIAL_CHARS));
     $lastName = trim(filter_var($_POST['lastName'], FILTER_SANITIZE_SPECIAL_CHARS));
+    $userName = trim(filter_var($_POST['userName'], FILTER_SANITIZE_SPECIAL_CHARS));
     $email = trim($_POST['email']);
     //check if email is invalid
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $errors[] = 'The email is not a valid email address.';
     }
-    $userName = trim(filter_var($_POST['userName'], FILTER_SANITIZE_SPECIAL_CHARS));
-    $pwd = trim(filter_var($_POST['pwd'], FILTER_SANITIZE_SPECIAL_CHARS));//DB vilka krav här?
-    $bio = trim(filter_var($_POST['bio'], FILTER_SANITIZE_SPECIAL_CHARS));
-    $avatar = $_FILES['avatar'];
-    //check if the avatar image is the right file type
-    if (!in_array($avatar['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
-        $errors[] = 'The file type is not allowed';
+    $password = filter_var($_POST['password']);
+    $hashed_pwd = password_hash($password, PASSWORD_DEFAULT);
+
+    //check if the user id is taken
+    $query = 'SELECT email FROM users WHERE email=:email';
+    $statement = $pdo->prepare($query);
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
     }
-    //check if the avatar image is the right file size, maximum 2MB
-    if($avatar['size'] > 2000000) {
-        $error[] = 'The file is larger than 2 megabyte.';
-    }
-    // If there are any errors in the array, upload the file. DB ska vi uploada el till db?
-    if (count($errors) === 0) {
-        $destination = __DIR__.'/uploads/'.$avatar['name'];//DB vilken är den korrekta sökvägen?
-    //upload file from the temporary path to a new destination.
-          move_uploaded_file($avatar['tmp_name'], $destination);
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->execute();
+
+    $email = $statement ->fetchAll(PDO::FETCH_ASSOC);
+    if ($query === $email) {
+        //take the user back to the login page
+        redirect("../../login.php");
     }
 
+    $query = 'INSERT INTO users(email, password, firstName, lastName, userName) VALUES (:email, :password, :firstName, :lastName, :userName )';
 
+    $statement = $pdo->prepare($query);
 
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
+    //Bind the parameter/argument to the variable.
+    $statement->bindParam(':firstName', $firstName, PDO::PARAM_STR);
+    $statement->bindParam(':lastName', $lastName, PDO::PARAM_STR);
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->bindParam(':userName', $userName, PDO::PARAM_STR);
+    $statement->bindParam(':password', $hashed_pwd, PDO::PARAM_STR);
+    // $statement->bindParam(':bio', $bio, PDO::PARAM_STR);
+    // $statement->bindParam(':avatar', $avatar['name'], PDO::PARAM_STR);
+    // When the SQL statement is prepared and all parameters are bound, the query with the execute function runs.
+    $statement->execute();
 
-    //include the database file.
-    //DB
-    //get the data from the asignup form.
-    $firstName = mysqli_real_escape_string($pdo, $_POST['firstName']);
-    $lastName = mysqli_real_escape_string($pdo, $_POST['lastName']);
-    $email = mysqli_real_escape_string($pdo, $_POST['email']);
-    $userName = mysqli_real_escape_string($pdo, $_POST['userName']);
-    $pwd = mysqli_real_escape_string($pdo, $_POST['pwd']);
-    $bio = mysqli_real_escape_string($pdo, $_POST['bio']);
-    $avatar = mysqli_real_escape_string($pdo, $_POST['avatar']);
-
-
-                //connect to database to check if the username already exist in the database
-                //DB
-                //return result from database
-                // $result = sqlite ????($pd, $sql);
-                //DB
-                //check if we have any results
-                // $resultCheck = sqlite????($result);
-                //DB
-                //check if the user id is taken
-                if ($resultCheck > 0) {
-                    redirect("../../signup.php");
-                } else {
-                    //if the user id does not exit, hash the password
-                    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-                    //Insert the user into the database
-                    //
-                    //take the user back to the login page
-                    redirect("../../login.php");
-                }
-            } else {
-    redirect("../../login.php");
-}
+    }
+    redirect('../../login.php');
